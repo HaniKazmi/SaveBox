f4c4192aa1d02ba9a36bf874ffea511ed4e0330e
Hani Kazmi <hanikazm@me.com>
2012-05-27 17:01:40 +0100

    Initial Commit

---
+++ SaveBox/SaveBox/SaveboxAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SaveboxAppDelegate.h
+//  SaveBox
+//
+//  Created by Hani Kazmi on 27/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SaveboxAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
